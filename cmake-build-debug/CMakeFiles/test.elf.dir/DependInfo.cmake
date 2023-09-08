adb9fe9e78d7e29a36e815a96327ca8efb40a869
LMTX <15368311317@163.com>
2023-09-08 16:53:36 +0800

    手动移植成功

---
+++ cmake-build-debug/CMakeFiles/test.elf.dir/DependInfo.cmake
@@ -0,0 +1,73 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/CubeMX/clion/FreeRTOS/test/Startup/startup_stm32f103c8tx.s" "E:/CubeMX/clion/FreeRTOS/test/cmake-build-debug/CMakeFiles/test.elf.dir/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/CubeMX/clion/FreeRTOS/test/Inc"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/CMSIS/Include"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/inc"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/portable/ARM_CM3"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj" "gcc" "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/portable/ARM_CM3/port.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/portable/ARM_CM3/port.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/portable/ARM_CM3/port.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/portable/MemMang/heap_4.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/portable/MemMang/heap_4.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/croutine.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/croutine.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/croutine.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/event_groups.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/event_groups.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/event_groups.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/list.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/list.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/list.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/queue.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/queue.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/queue.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/stream_buffer.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/stream_buffer.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/stream_buffer.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/tasks.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/tasks.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/tasks.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/FreeRTOS_M3_gcc/src/timers.c" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/timers.c.obj" "gcc" "CMakeFiles/test.elf.dir/FreeRTOS_M3_gcc/src/timers.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/gpio.c" "CMakeFiles/test.elf.dir/Src/gpio.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/gpio.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/main.c" "CMakeFiles/test.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/main.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/stm32f1xx_hal_msp.c" "CMakeFiles/test.elf.dir/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/stm32f1xx_hal_msp.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/stm32f1xx_it.c" "CMakeFiles/test.elf.dir/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/stm32f1xx_it.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/syscalls.c" "CMakeFiles/test.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/syscalls.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/sysmem.c" "CMakeFiles/test.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/sysmem.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/system_stm32f1xx.c" "CMakeFiles/test.elf.dir/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/system_stm32f1xx.c.obj.d"
+  "E:/CubeMX/clion/FreeRTOS/test/Src/usart.c" "CMakeFiles/test.elf.dir/Src/usart.c.obj" "gcc" "CMakeFiles/test.elf.dir/Src/usart.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
